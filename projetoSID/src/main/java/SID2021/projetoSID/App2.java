f9442aeba9afccf1d6acfffd701ac067059dc199
pshoa-shtranha <pshoa@iscte-iul.pt>
2021-03-06 01:53:02 +0000

    p

--- projetoSID/src/main/java/SID2021/projetoSID/App2.java
+++ projetoSID/src/main/java/SID2021/projetoSID/App2.java
@@ -1,35 +1,43 @@
 package SID2021.projetoSID;
 
 import com.mongodb.client.*;
-
+import org.bson.Document;
 
 public class App2 {
 	static String connectionString = "mongodb://aluno:aluno@194.210.86.10:27017/?authSource=admin";
 
+    private static final int MAX_REGISTERS = 5000;
+	
 	public static void main(String[] args) {
 		try (MongoClient mongoClient = MongoClients.create(connectionString)) {
 
 			MongoDatabase db = mongoClient.getDatabase("sid2021");
-			MongoCollection mongoCollection = db.getCollection("sensorh1");
+			MongoCollection<Document> mongoCollection = db.getCollection("sensorh1");
 			
-			  printDBNames(mongoClient); 
-			  printCollections(db);
-			  printNumDocs(mongoCollection);
-			 
-			
-//			  Document objeto = new ; FindIterable<Document> objetos =
-//			  mongoCollection.find();
-//			  objetos.forEach(new Block<Document>() { public void apply(final Document
-//			  document) { System.out.println(document.toJson()); } }
+			printDocsInCollection(mongoCollection);
+//			  printDBNames(mongoClient); 
+//			  printCollections(db);
+//			  printNumDocs(mongoCollection);
 			 
 		} catch (Exception e) {
 			System.out.println(e);
 		}
 	}
 
+	private static void printDocsInCollection(MongoCollection<Document> mongoCollection) {
+		MongoCursor<Document> docCursor = mongoCollection.find().iterator();
+		
+		int count = 0;
+		while (docCursor.hasNext() && count < MAX_REGISTERS) {
+		    Document doc = docCursor.next();
+		    System.out.println(doc.toJson());
+		    
+		    count++;
+		}
+	}
+
 	private static void printNumDocs(MongoCollection mongoCollection) {
-		long numdocs = mongoCollection.countDocuments();
-		System.out.println(numdocs);
+		System.out.println(mongoCollection.countDocuments());
 	}
 
 	private static void printCollections(MongoDatabase db) {
